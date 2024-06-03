bf36b3a5b86fd943600a96c6b95b7ca4a79c16fd
Никита <153456515+Lairont@users.noreply.github.com>
2024-06-03 14:36:44 +0300

    Add files via upload

---
+++ 03ю06/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03ю06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //зад.40
+            //    for (int x=1;x<=11;x+=2)
+            //{
+            //    if (x != 3)
+            //    {
+            //    Console.Write($"{x}\t");
+            //    }
+            //}
+            //    Console.ReadLine();
+            //зад.41
+            //    for (int x = 4; x <= 20; x += 4)
+            //{
+            //        Console.Write($"{x}\t");
+            //}
+            //Console.ReadLine();
+            //зад.43
+            //for (int x = 10; x <=99; x ++)
+            //{
+            //    if (x % 10==0)
+            //    { 
+            //    Console.Write($"{x}\t");
+            //    }
+            //}
+            //Console.ReadLine();
+            //зад.44
+            //for (int x = 100; x <= 200; x++)
+            //{
+            //    if (x % 3 == 0)
+            //    {
+            //        Console.WriteLine($"{x}");
+            //    }
+            //}
+            //Console.ReadLine();
+            //зад.45
+            //double x = 20.4;
+            //double g = 0;
+            //while(g<=20)
+            //{
+            //    g++; 
+            //        Console.WriteLine($"{g}шт={x*g}руб");
+            //}
+            //Console.ReadLine();
+            //зад.46
+            //int x = 0;
+            //for (int x=0;x<=20;x++)
+            //{
+            //    Console.WriteLine("20");
+            //}
+            //Console.ReadLine();
+            //зад.23
+            //Random rnd = new Random();
+            //for (int i=0; i<10; i++)
+            //{
+            //    int x = rnd.Next(-30, 30);
+            //    if (x>0)
+            //    {
+            //        Console.WriteLine($"{x} - положительное");
+            //    }
+            //    else
+            //    {     
+            //        Console.WriteLine($"{x} - отрицательное");
+            //    }
+            //}
+            //Console.ReadLine();
+            //зад.24
+            //Random rnd = new Random();
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int x = rnd.Next(-20, 30);
+            //    if (x % 2 == 0)
+            //    {
+            //        Console.WriteLine($"{x} - четное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{x} - нечетное");
+            //    }
+            //}
+            //Console.ReadLine();
+            //зад.25
+            //Random rnd = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    int x = rnd.Next(-20, 20);
+            //    if (x>=0)
+            //    if (x % 2 == 0)
+            //    {
+            //        Console.WriteLine($"{x} - четное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{x} - нечетное");
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+        }
+    }
+}
